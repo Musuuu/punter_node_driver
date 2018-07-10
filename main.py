0814467ea877ef9331df0038a511f4a7df759726
Musuuu <crm.marco@gmail.com>
2018-07-10 11:47:30 +0200

    Add potentiometer process

--- main.py
+++ main.py
@@ -70,6 +70,13 @@ def api_reader(queue, controller):
         controller.api_stop()
 
 
+def potentiometer_reader(queue):
+    """Read the position of the potentiometer"""
+    position = queue.get()
+    # ......
+
+    return position
+
 def set_engine(queue):
     """Give commands to the engine queue"""
 
@@ -82,27 +89,14 @@ def writer(count, queue):
 
 
 if __name__ == '__main__':
-    engine_q = Queue()
-    engine = Stepper(0, 1, 2, 3)
+    engine_q, api_q, potentiometer_q = Queue()
 
+    engine = Stepper(0, 1, 2, 3)
     controller = Controller()
     controller.engine_pointer = engine
 
-    api_q = Queue()
-
     api_reader_p = Process(target=api_reader, args=(api_q, controller,))
+    potentiometer_reader_p = Process(target=potentiometer_reader, args=(potentiometer_q,))
 
     while True:
         api_reader()
-#     for count in [10**4, 10**5, 10**6]:
-#         queue = Queue()   # reader() reads from queue
-#                           # writer() writes to queue
-#         reader_p = Process(target=reader, args=((queue),))
-#         reader_p.daemon = True
-#         reader_p.start()        # Launch reader() as a separate python process
-#
-#         _start = time.time()
-#         writer(count, queue)    # Send a lot of stuff to reader()
-#         reader_p.join()         # Wait for the reader to finish
-#         print "Sending %s numbers to Queue() took %s seconds" % (count,
-#             (time.time() - _start))
